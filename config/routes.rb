222fdd7395ee3bf82f8e5626f0020dd6f0c1731a
石本桂揮 <keikiishimoto@ishimotokatsurakinoMacBook-Air.local>
2019-11-19 12:10:42 -0500

    Added login namesapce and boostrap styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  resources :posts
+
+  devise_for :users
+  resources :posts, only: [ :index, :show, :update ]
+
+  root to: 'posts#index'
+  namespace :login do
+    root to: 'posts#index'
+    resources :posts
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
